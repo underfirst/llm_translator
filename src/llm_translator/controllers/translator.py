6733e5a7d6ee660100c2ba0d54ce7261d774a9f5
reiven <c.t.reibun@gmail.com>
2025-03-23 00:05:37 +0900

    feat: Add translation and text processing functions for LLM usage

--- src/llm_translator/controllers/translator.py
+++ src/llm_translator/controllers/translator.py
@@ -1,14 +1,64 @@
-from vllm import LLM
+import numpy as np
+from transformers import AutoTokenizer
+from vllm import LLM, SamplingParams
+
+MODEL_NAME = "Qwen/Qwen2.5-7B-Instruct"
+MAX_LENGTH = 1000
+MAX_INPUT_LENGTH = round(MAX_LENGTH * 0.35)
 
 
-def translate_en_to_ja(text: str) -> str:
-    client = LLM(
-        model="Qwen/Qwen2.5-7B-Instruct"
-    )
+client = LLM(model=MODEL_NAME)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-    prompt = f"Translate the following English text to Japanese:\n{text}"
-    response = client.generate(prompt=prompt, max_tokens=100)
+
+def translate_en_to_ja(text: str, max_length: int = MAX_LENGTH) -> str:
+    params = SamplingParams(max_tokens=max_length, temperature=0.9, top_p=0.95)
+
+    prompt = f"Translate the following English text to Japanese:\n\n{text}"
+    response = client.generate(prompt=prompt, sampling_params=params)
     return response[0].text.strip()
+
+
+def get_token_length(text: str) -> int:
+    return tokenizer([text], return_tensors="pt")["input_ids"].shape[1]
+
+
+def check_max_token_length(text: str) -> int:
+    paragraphs = text.split("\n")
+
+    lengths = []
+    for paragraph in paragraphs:
+        length = get_token_length(paragraph)
+        lengths.append(length)
+    return np.max(lengths)
+
+
+def split_text(text: str) -> list[str]:
+    max_paragraph_length = check_max_token_length(text)
+    if max_paragraph_length < MAX_INPUT_LENGTH:
+        max_paragraph_length = MAX_INPUT_LENGTH
+
+    paragraphs = []
+
+    for paragraph in text.split("\n\n"):
+        if paragraph.strip() == "":
+            paragraphs.append(paragraph)
+        else:
+            paragraph_length = get_token_length(paragraph)
+            if paragraph_length < MAX_INPUT_LENGTH:
+                paragraphs.append(paragraph)
+            else:
+                # TODO: split sentence to fullfill MAX_INPUT_LENGTH AI!
+                ...
+
+    return paragraphs
+
+
+def translate(text: str) -> str:
+    # split text to paragraphs.
+    # for paragraph in paragraphs: check the paragraph token length < 35% of max_length:
+    # if longer than 35%, split paragraph to sentence.
+    pass
 
 
 if __name__ == "__main__":
